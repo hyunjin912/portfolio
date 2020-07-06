cc7e8965bcec3d07b7acb33f78f4d7d66de21416
hyunjin912 <65323375+hyunjin912@users.noreply.github.com>
2020-07-07 01:39:57 +0900

    2020.07.07

--- port/js/custom2.js
+++ port/js/custom2.js
@@ -5,7 +5,7 @@ const cursor = $(".cursor")
 let circleWidth = cursor.width();
 let circleHeight = cursor.height();
 
-$("body").on("mousemove", function(e){
+$("#header").on("mousemove", function(e){
     gsap.to(cursor,{duration: 0.3, left: e.pageX - (circleWidth / 2), top: e.pageY - (circleHeight / 2)});
 });
 
@@ -16,3 +16,13 @@ $(".split").each(function () {
     split = "<span aria-hidden='true'>" + split + "</span>";
     $(this).html(split).attr("aria-label", txt);
 });
+
+setTimeout(function(){
+    gsap.to(".header-title h1 strong span", 0.6, {ease: Back.easeOut.config(1.7), opacity: 1, y: 0, stagger: 0.05});
+    gsap.to(".header-title h1 em span", 0.6, {ease: Back.easeOut.config(1.7), opacity: 1, y: 0, stagger: 0.05, delay: 0.5});
+    gsap.to(".sub-tit span", 0.6, {ease: Back.easeOut.config(1.7), opacity: 1, stagger: 0.05, delay: 1.5});
+    gsap.to(".desc", 0.6, {ease: Back.easeOut.config(1.7), opacity: 1, y: 0, stagger: 0.05, delay: 3.5});
+    // gsap.to("#header", 2, {ease: Back.easeOut.config(1.7), background: "#1b1b1b", zIndex: -1, delay: 3.5});
+    gsap.to(".cursor", 0.8, {ease: Back.easeOut.config(1.7), width: 200, height: 200, delay: 4.5});
+},2000);
+
