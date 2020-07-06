ac2c2b646b388d54326d84d91e5c7b83879fe0cc
hyunjin912 <65323375+hyunjin912@users.noreply.github.com>
2020-07-07 02:10:48 +0900

    2020.07.07

--- port/js/custom2.js
+++ port/js/custom2.js
@@ -5,7 +5,7 @@ const cursor = $(".cursor")
 let circleWidth = cursor.width();
 let circleHeight = cursor.height();
 
-$("#header").on("mousemove", function(e){
+$("body").on("mousemove", function(e){
     gsap.to(cursor,{duration: 0.3, left: e.pageX - (circleWidth / 2), top: e.pageY - (circleHeight / 2)});
 });
 
@@ -22,7 +22,5 @@ setTimeout(function(){
     gsap.to(".header-title h1 em span", 0.6, {ease: Back.easeOut.config(1.7), opacity: 1, y: 0, stagger: 0.05, delay: 0.5});
     gsap.to(".sub-tit span", 0.6, {ease: Back.easeOut.config(1.7), opacity: 1, stagger: 0.05, delay: 1.5});
     gsap.to(".desc", 0.6, {ease: Back.easeOut.config(1.7), opacity: 1, y: 0, stagger: 0.05, delay: 3.5});
-    // gsap.to("#header", 2, {ease: Back.easeOut.config(1.7), background: "#1b1b1b", zIndex: -1, delay: 3.5});
-    gsap.to(".cursor", 0.8, {ease: Back.easeOut.config(1.7), width: 200, height: 200, delay: 4.5});
 },2000);
 
