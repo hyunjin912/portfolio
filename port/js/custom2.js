628f3ad37eadbef3e70323ca544eb40a65dcc200
hyunjin912 <skyhyunjinlee@gmail.com>
2020-07-06 16:16:48 +0900

    2020.07.06

---
+++ port/js/custom2.js
@@ -0,0 +1,10 @@
+/* cursor */
+const cursor = $(".cursor")
+
+// 원의 크기를 구해서 반지름 값을 변수화
+let circleWidth = cursor.width();
+let circleHeight = cursor.height();
+
+$("body").on("mousemove", function(e){
+    gsap.to(cursor,{duration: 0.3, left: e.pageX - (circleWidth / 2), top: e.pageY - (circleHeight / 2)});
+});
