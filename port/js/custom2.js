7b499f80678af591517434cf55faa9c4bc7e3284
hyunjin912 <65323375+hyunjin912@users.noreply.github.com>
2020-07-06 23:57:21 +0900

    2020.07.06

--- port/js/custom2.js
+++ port/js/custom2.js
@@ -8,3 +8,11 @@ let circleHeight = cursor.height();
 $("body").on("mousemove", function(e){
     gsap.to(cursor,{duration: 0.3, left: e.pageX - (circleWidth / 2), top: e.pageY - (circleHeight / 2)});
 });
+
+/* split */
+$(".split").each(function () {
+    let txt = $(this).text();
+    let split = txt.split("").join("</span><span aria-hidden='true'>");
+    split = "<span aria-hidden='true'>" + split + "</span>";
+    $(this).html(split).attr("aria-label", txt);
+});
